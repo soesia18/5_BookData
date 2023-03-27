c9c78477ba836615f47cd061a854fe2ee3fce1f5
simon <simon.schoeggler@hotmail.com>
2023-03-27 19:08:10 +0200

    Initial commit

---
+++ src/main/java/at/kaindorf/pojos/Author.java
@@ -0,0 +1,12 @@
+package at.kaindorf.pojos;
+
+/**
+ * <h3>Created by IntelliJ IDEA.</h3><br>
+ * <b>Project:</b> BookData<br>
+ * <b>User:</b> Simon Schoeggler<br>
+ * <b>Date:</b> 22. März 2023<br>
+ * <b>Time:</b> 09:10<br>
+ */
+
+public class Auther {
+}
---
+++ src/main/java/at/kaindorf/xml/XML_Access.java
@@ -0,0 +1,11 @@
+package at.kaindorf.xml;
+/**
+* <h3>Created by IntelliJ IDEA.</h3><br>
+* <b>Project:</b> BookData<br>
+* <b>User:</b> Simon Schoeggler<br>
+* <b>Date:</b> 22. März 2023<br>
+* <b>Time:</b> 09:15<br>
+*/
+
+public class XML_Access {
+}
